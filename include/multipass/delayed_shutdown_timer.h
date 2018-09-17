94b025be9719bde5ccac8cf32556f2cd889a2e83
Chris Townsend <christopher.townsend@canonical.com>
2018-09-17 14:23:34 +0200

    daemon: Implement a class for delayed shutdown handling

---
+++ include/multipass/delayed_shutdown_timer.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_DELAYED_SHUTDOWN_TIMER_H
+#define MULTIPASS_DELAYED_SHUTDOWN_TIMER_H
+
+#include <multipass/virtual_machine.h>
+
+#include <QObject>
+#include <QTimer>
+
+#include <chrono>
+
+namespace multipass
+{
+class DelayedShutdownTimer : public QObject
+{
+    Q_OBJECT
+
+public:
+    DelayedShutdownTimer(VirtualMachine* virtual_machine, std::chrono::milliseconds delay);
+    ~DelayedShutdownTimer();
+
+    void cancel_delayed_shutdown();
+
+signals:
+    void finished();
+
+private:
+    void start_timer();
+    void shutdown_instance();
+
+    QTimer shutdown_timer;
+    VirtualMachine* virtual_machine;
+    std::chrono::milliseconds delay;
+};
+} // namespace multipass
+#endif // MULTIPASS_DELAYED_SHUTDOWN_TIMER_H
